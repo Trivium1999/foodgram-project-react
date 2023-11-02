8c43858bcf6550b7b99affac240dc0eec4b15827
Анна Романова <little4one@yandex.ru>
2023-11-02 16:51:58 +0700

    подготовка к деплою

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-mzg57ft$y0p2f)b##djf++mg9k+i472zgu%u_@jp7uo=8n-@5$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.0.113', '127.0.0.1', 'localhost', 'foodgram-little4one.sytes.net']
 
 
 # Application definition
@@ -72,10 +72,15 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
+
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -136,6 +141,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
